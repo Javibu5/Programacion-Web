aa411757be6ab758b27854058a64837898ce2325
Javibu5 <adfjb77@gmail.com>
2018-10-29 12:01:50 +0100

    28/10/18

---
+++ Practica 2/detallesEmpleados.php
@@ -0,0 +1,38 @@
+
+<?php
+    //require_once('queryFunctions.php');
+ 
+   $host = "127.0.0.1";
+   $username = "root";
+   $password = "secret";
+   $database = "EjemploPW";
+   
+$conn = new mysqli ($host, $username, $password, $database);
+  if ($conn->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo $mysqli->host_info . "\n";
+}
+
+  $sql ="SELECT * from empleados where dni='" . $_GET['dni'] . "'";
+
+   $rows = $conn->query($sql);
+   if (!$rows)
+    die ($conn->error);
+   
+   $row=$rows->fetch_assoc();
+  ?>
+
+  <p><strong>Nombre: </strong><?php echo $row["nombre"] ?></p> 
+  <p><strong>Apellidos: </strong><?php echo $row["apellido"]?></p> 
+  <p><strong>dni: </strong><?php echo $row["dni"]?></p> 
+  <p><strong>direccion: </strong><?php echo $row["direccion"]?></p> 
+  <p><strong>anoNacimiento </strong><?php echo $row["anoNacimiento"]?></p> 
+  <p><strong>departamento: </strong><?php echo $row["departamento"]?></p> 
+
+ <br/>
+
+<a href="/index.php">Inicio</a>
+
+<?php
+   $conn = null;
+  ?>
---
+++ Practica 2/index.php
@@ -0,0 +1,53 @@
+
+<head>
+<title>La web</title>
+</head>
+
+<style>
+td, th {
+       border: 1px solid #dddddd;
+       text-align: left;
+       padding: 8px;
+        }
+</style>
+
+<?php
+    //require_once('queryFunctions.php');
+ 
+   $host = "127.0.0.1";
+   $username = "root";
+   $password = "secret";
+   $database = "EjemploPW";
+   
+$conn = new mysqli ($host, $username, $password, $database);
+if ($conn->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo $mysqli->host_info . "\n";
+}
+
+$sql = "SELECT nombre, dni from empleados"
+
+?>
+
+
+<h1>Empleados</h1>
+<table>
+  <tr>
+    <th>Nombre</th>
+    <th>Enlaces</th>
+  </tr>
+
+
+<?php
+   $rows = $conn->query($sql);
+   if (!$rows)
+    die ($conn->error);
+   foreach ($rows as $row)
+    {
+echo '<tr>
+            <td>'.$row["nombre"].'</td>
+      <td><a href="detallesEmpleados.php?dni='.urldecode($row["dni"]).'">Ver detalles</a></td></tr>';
+    }
+   echo "</table>";
+   $conn = null;
+  ?>
